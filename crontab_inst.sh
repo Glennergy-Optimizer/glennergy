f767d4858a8c0e5c45eb27b1a380d98e30afdec6
Hbitz <53080862+Hbitz@users.noreply.github.com>
2026-07-08 19:00:59 +0200

    Update cron job commands to include locking and logging for Glennergy scripts

--- crontab_inst.sh
+++ crontab_inst.sh
@@ -5,8 +5,8 @@ case "$1" in
   add)
     /usr/bin/crontab -l 2>/dev/null | grep -v "# Glennergy" | {
       cat
-      echo "* * * * * /usr/local/bin/Glennergy-Spotpris # Glennergy-Spotpris cron"
-      echo "* * * * * /usr/local/bin/Glennergy-Meteo # Glennergy-Meteo cron"
+      echo "* * * * * flock -n /tmp/glennergy-meteo.lock sh -c 'umask 0002; timeout 45s /usr/local/bin/Glennergy-Meteo >> /var/log/glennergy/meteo-cron.log 2>&1' # Glennergy-Meteo cron"
+      echo "* * * * * flock -n /tmp/glennergy-spotpris.lock sh -c 'umask 0002; timeout 45s /usr/local/bin/Glennergy-Spotpris >> /var/log/glennergy/spotpris-cron.log 2>&1' # Glennergy-Spotpris cron"
     } | /usr/bin/crontab -
     echo "Glennergy cron jobs added."
     ;;
